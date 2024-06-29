017697288c47e91d772bc339b9615db05f1fe4fc
bingehacker <mazianewassim@gmail.com>
2024-06-29 02:34:47 +0100

    Fixed multibyte character handling

--- wc_tool/main.cpp
+++ wc_tool/main.cpp
@@ -4,19 +4,29 @@
 #include <cwchar>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 streamsize byteCount(string filename) {
-  ifstream file;
-  streamsize size;
-  file.open(filename, ios::in | ios::binary | ios::ate);
-  if (file.is_open()) {
-    size = file.tellg();
-    file.close();
-    return size;
+  if (filename == "") {
+    FILE *fp = stdin;
+    int count = 0;
+    char chr = fgetc(fp);
+    while (chr != EOF) {
+      count++;
+      chr = fgetc(fp);
+    }
+    return count;
+  } else {
+    ifstream file;
+    streamsize size;
+    file.open(filename, ios::in | ios::binary | ios::ate);
+    if (file.is_open()) {
+      size = file.tellg();
+      file.close();
+      return size;
+    } else
+      return -1;
   }
-
-  else
-    return -1;
 }
 
 int characterCount(string filename) {
@@ -35,7 +45,7 @@ int characterCount(string filename) {
 
 int wideCharacterCount(string filename) {
   setlocale(LC_CTYPE, "C.utf8");
-  FILE *fp = fopen(filename.c_str(), "r");
+  FILE *fp = (filename != "") ? fopen(filename.c_str(), "r") : stdin;
   int count = 0;
   wchar_t countingObject;
   if (fp == NULL)
@@ -52,69 +62,80 @@ int wideCharacterCount(string filename) {
 }
 
 int wordCount(string filename) {
-  ifstream file;
   string countingObject;
-  int count = 0;
-  file.open(filename);
-  if (file.is_open()) {
-    while (file >> countingObject)
+  if (filename == "") {
+    int count = 0;
+    while (cin >> countingObject)
       count++;
-    file.close();
     return count;
-  } else
-    return -1;
+  } else {
+    ifstream file;
+    int count = 0;
+    file.open(filename);
+    if (file.is_open()) {
+      while (file >> countingObject)
+        count++;
+      file.close();
+      return count;
+    } else
+      return -1;
+  }
 }
 
 int lineCount(string filename) {
-  ifstream file;
-  string countingObject;
-  int count = 0;
-  file.open(filename);
-  if (file.is_open()) {
-    while (getline(file, countingObject))
+  if (filename == "") {
+    int count = 0;
+    string countingObject;
+    while (getline(cin, countingObject))
       count++;
-    file.close();
     return count;
-  } else
-    return -1;
+  } else {
+    ifstream file;
+    string countingObject;
+    int count = 0;
+    file.open(filename);
+    if (file.is_open()) {
+      while (getline(file, countingObject))
+        count++;
+      file.close();
+      return count;
+    } else
+      return -1;
+  }
 }
 
 int main(int argc, char *argv[]) {
+  vector<string> args(argv, argv + argc);
   set<char> flags = {'c', 'w', 'l', 'm'};
   string filename;
   int WCount, LCount, CCount;
   streamsize size;
 
   if (argc == 1) {
-    cerr << "Usage : wctool [flag] <filename>";
-    return 1;
-  }
-
-  filename = argv[1];
-  if (argc == 2) {
-    if (filename[0] != '-') {
-      size = byteCount(filename);
-      WCount = wordCount(filename);
-      LCount = lineCount(filename);
-      if (size == -1 || WCount == -1 || LCount == -1) {
-        {
-          cout << "Failed to open file" << '\n';
-          return -1;
-        }
-      } else {
-        cout << LCount << ' ' << WCount << ' ' << size << ' ' << filename
-             << '\n';
-        return 0;
+    WCount = wordCount(filename);
+    LCount = lineCount(filename);
+    size = byteCount(filename);
+    if (size == -1 || WCount == -1 || LCount == -1) {
+      {
+        cout << "Failed to open file" << '\n';
+        return -1;
       }
+    } else {
+      cout << LCount << ' ' << WCount << ' ' << size << ' ' << filename << '\n';
+      return 0;
     }
   }
 
-  if (argc != 3 || argv[1][0] != '-') {
+  if (argc > 3) {
     cerr << "Usage : wctool [flag] <filename>" << '\n';
     return 1;
   }
-  filename = argv[2];
-  switch (argv[1][1]) {
+  try {
+    filename = args.at(2);
+  } catch (const out_of_range &e) {
+    filename = "";
+  }
+  switch (args[1][1]) {
   case 'c': {
     size = byteCount(filename);
     if (size != -1) {
@@ -163,7 +184,8 @@ int main(int argc, char *argv[]) {
     cout << "Valid Flags : \n"
          << "\t-c : byte count \n"
          << "\t-l : line count \n"
-         << "\t-w : word count \n";
+         << "\t-w : word count \n"
+         << "\t-m : character count \n";
     break;
   }
 }
